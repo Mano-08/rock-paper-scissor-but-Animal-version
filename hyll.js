59a4fe9d90818bddb854f0f51e455260b0f1a289
Mano <mano.26q@gmail.com>
2022-03-11 14:30:32 +0530

    Updated

--- hyll.js
+++ hyll.js
@@ -1,7 +1,7 @@
 var matches_won_by_user=0;
 var matches_won_by_computer=0;
 var available_options=["mouse","cat","elephant"];
-var random_number=Math.floor(Math.random() * 3);
+var random_number=Math.floor(Math.random() * 3)+1;
 
 
 
