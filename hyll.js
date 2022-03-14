aa8dbfe05d33266a47c8473352fba2cacfbf8f18
Mano <mano.26q@gmail.com>
2022-03-14 16:07:51 +0530

    Updated
    Added new result outlet (with images) successfully

--- hyll.js
+++ hyll.js
@@ -5,17 +5,6 @@ var user_random_number = 0;
 
         
 
-function the_game(a) {
-    if (a===1) {
-        
-    }
-    if (a===2) {
-        document.querySelector(".user-choice-display").setAttribute("src", "images/img2.png");
-    }
-    if (a===3) {
-        document.querySelector(".user-choice-display").setAttribute("src", "images/img3.png");
-    }
-}
 
 function the_computer(b) {
     var random_number=Math.floor( Math.random() * 3 ) + 1;
@@ -36,25 +25,54 @@ function the_computer(b) {
         }
     }
     
-    var random_image_obtained = "images/img" + random_number + ".png";
+    if (b===2) {
+        if (random_number===2) {
+            document.querySelector(".user-choice-display").setAttribute("src", "Results/cat_happy_left.png");
+            document.querySelector(".computer-choice-display").setAttribute("src", "Results/cat_happy.png");
+        }
+
+        if (random_number===3) {
+            document.querySelector(".user-choice-display").setAttribute("src", "Results/cat_left.png");
+            document.querySelector(".computer-choice-display").setAttribute("src", "Results/elephant_right.png");
+        }
+
+        if (random_number===1) {
+            document.querySelector(".user-choice-display").setAttribute("src", "Results/cat_happy_left.png");
+            document.querySelector(".computer-choice-display").setAttribute("src", "Results/mice_scared_right.png");
+        }
+    }
+    
+    if (b===3) {
+        if (random_number===2) {
+            document.querySelector(".user-choice-display").setAttribute("src", "Results/elephant_left.png");
+            document.querySelector(".computer-choice-display").setAttribute("src", "Results/cat_right.png");
+        }
+
+        if (random_number===3) {
+            document.querySelector(".user-choice-display").setAttribute("src", "Results/elephant_left.png");
+            document.querySelector(".computer-choice-display").setAttribute("src", "Results/elephant_right.png");
+        }
+
+        if (random_number===1) {
+            document.querySelector(".user-choice-display").setAttribute("src", "Results/elephant_left.png");
+            document.querySelector(".computer-choice-display").setAttribute("src", "Results/mice_scared_right.png");
+        }
+    }
     
 }
 
 function cat_respond() {
     user_random_number = 2;
-    the_game(user_random_number);
     the_computer(user_random_number);
 }
 
 function mouse_respond() {
     user_random_number = 1;
-    the_game(user_random_number);
     the_computer(user_random_number);
 }
 
 function elephant_respond() {
     user_random_number = 3;
-    the_game(user_random_number);
     the_computer(user_random_number);
 }
 
