53037544b9480a1e652d8dc1fcd9957328e0e03d
Mano <mano.26q@gmail.com>
2022-06-22 15:25:53 +0530

    Update hyll.js

--- hyll.js
+++ hyll.js
@@ -209,38 +209,3 @@ function elephant_respond() {
     disable_onclick()
 }
 
-
-
-/*
-document.addEventListener("contextmenu", function (e){
-    e.preventDefault();
-}, false);
-
-
-
-
-<script>
-   let count=0
-
-   let bount=0
-
-   let countEl=document.getElementById("count-el")
-   let incrementEl=document.getElementById("increment-btn")
-   let saveEl=document.getElementById("save-el")
-   let totalEl=document.getElementById("total-el")
-
-   function increment() {
-     count+=1
-     bount+=1
-     totalEl.textContent=bount
-     countEl.textContent=count
-   }
-
-
-   function save() {
-     saveEl.textContent+= count + " - "
-     count=0
-     countEl.textContent=0
-   }
-  </script>
-  */
